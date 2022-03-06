78f1eb224062c91e4e74b28aecd40d5e2f683878
mrsamia <15203060@iubat.edu>
2022-03-06 17:20:28 +0600

    SRM | accordion

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,10 +1,39 @@
-import React from 'react';
+
+import '../../Style/accordion.css';
+import React, { useState } from 'react';
 
 function Home(props) {
+
+    const accordionData = {
+        title: 'Section 1',
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.`
+    };
+    const { title, content } = accordionData;
+    const [isActive, setIsActive] = useState(false);
+
     return (
-        <div>
+        <div className='Container'>
             <h2>This is Home Page</h2>
             <button className='btn btn-primary'>button</button>
+
+
+            <h1>React Accordion Demo</h1>
+            <div className="accordion">
+                <div className="accordion-item">
+                    <div
+                        className="accordion-title"
+                        onClick={() => setIsActive(!isActive)}
+                    >
+                        <div>{title}</div>
+                        <div>{isActive ? '-' : '+'}</div>
+                    </div>
+                    {isActive && <div className="accordion-content">{content}</div>}
+                </div>
+            </div>
+
         </div>
     );
 }
