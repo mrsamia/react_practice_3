35cc11f1a8988d572f1154fa04c2d803d466fd80
mrsamia <15203060@iubat.edu>
2022-03-11 00:03:05 +0600

    SRM| form_update

--- src/Components/SingleCompnt/InputForm.js
+++ src/Components/SingleCompnt/InputForm.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 import Input from './Input';
-
+import validator from 'validator'
 import Button from '../SingleCompnt/Button';
 import {Link} from 'react-router-dom';
 
 
 function InputForm(props) {
 
+    // const [emailError, setEmailError] = useState('')
+    // const validateEmail = (e) => {
+    //   var email = e.target.value
+    
+    //   if (validator.isEmail(email)) {
+    //     setEmailError('Valid Email :)')
+    //   } else {
+    //     setEmailError('Enter valid Email!')
+    //   }
+    // }
 
 
 
     return (
         <div>
+            <div className="pt-5 pb-4 ">
+                <img src={require("../Assets/Group.png")}/>
+            </div>
+
             <Input name='username' placeholder="Username"/>
+            
+            {/* <Input name='email' placeholder="Email" type="text" id="userEmail" onChange={(e) => validateEmail(e)}/>
+            <p style={{fontWeight: 'bold', color: 'red'}}>{emailError}</p> */}
+
             <Input  name='password' placeholder="Password"/><br></br>
             
             <Link to="/Welcome" className="btn btn-primary btn-style">LOGIN</Link>
